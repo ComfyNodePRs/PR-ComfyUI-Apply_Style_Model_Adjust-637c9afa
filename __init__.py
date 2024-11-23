3fcb14ae17e1559b68131e2b24e39d78b3096fe9
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-11-23 05:50:27 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+print("Loaded Apply Style Model Adjust node - Use this for better control over style vs prompt balance")
